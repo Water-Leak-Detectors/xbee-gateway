73d3fe6cf234a29b3f2a08c1bed296b54f4db85c
PooyaKhandel <pooya_khandel@omidtech.local>
2017-10-11 20:38:14 +0330

    second node is added

--- client.py
+++ client.py
@@ -9,27 +9,49 @@ which arrive to appropriate methods for processing in a separate thread.
 from xbee.thread import XBee, ZigBee
 from xbee.helpers.dispatch import Dispatch
 import serial
+import struct
 import paho.mqtt.client as mqtt
 import time
 import json
 
 # Create handlers for various packet types
+
+
 def status_handler(name, packet):
     print("Status Update - Status is now: {}".format(packet['status']))
-    sensor_data['fsr'] = 4000
-    client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+    # sensor_data_1['fsrN1'] = 4000
+    # client.publish('v1/devices/me/telemetry', json.dumps(sensor_data_1), 1)
+    # sensor_data_2['fsrN2'] = 2000
+    # client_2.publish('v1/devices/me/telemetry', json.dumps(sensor_data_2), 1)
+
 
 def io_sample_handler(name, packet):
     print("Samples Received: {}".format(packet['samples']))
 
+
 def receive_packet(name, packet):
+    sensor_data_1 = {'fsrN1': 0}
+    sensor_data_2 = {'fsrN2': 0}
     print(packet)
-    print("rf_data: {}".format(packet['rf_data']))
-    sensor_data['fsr'] = int.from_bytes(packet['rf_data'], 'big')
-    print("sensor_data: {}".format(sensor_data))
+    rf_data = struct.unpack('>BBBHB', packet['rf_data'])
+    data_type = chr(rf_data[0])
+    data_node = chr(rf_data[1])
+    data = rf_data[3]
+    if data_type == 'f':
+        if data_node == '1':
+            sensor_data_1['fsrN1'] = data
+            print(data_type + ' ' + data_node + ' ' + '{}'.format(sensor_data_1['fsrN1']))
+            client.publish('v1/devices/me/telemetry', json.dumps(sensor_data_1), 1)
+        elif data_node == '2':
+            sensor_data_2['fsrN2'] = data
+            print(data_type + ' ' + data_node + ' ' + '{}'.format(sensor_data_2['fsrN2']))
+            client.publish('v1/devices/me/telemetry', json.dumps(sensor_data_2), 1)
+    # print("rf_data: {}".format(packet['rf_data']))
+    # sensor_data_1['fsrN1'] = int.from_bytes(packet['rf_data'], 'big')
+    # print("sensor_data: {}".format(sensor_data_1))
     # time.sleep(2)
     # Sending humidity and temperature data to ThingsBoard
-    client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+    # client.publish('v1/devices/me/telemetry', json.dumps(sensor_data_1), 1)
 
     # next_reading += INTERVAL
     # sleep_time = next_reading - time.time()
@@ -38,7 +60,8 @@ def receive_packet(name, packet):
     # pass
 
 THINGSBOARD_HOST = '172.25.224.84'
-ACCESS_TOKEN = 'sXEes3EvtyUZJkreuRCb'
+ACCESS_TOKEN_1 = 'sXEes3EvtyUZJkreuRCb'
+ACCESS_TOKEN_2 = 'Y1PBluwmhe3UfLLPSAg7'
 # ACCESS_TOKEN = '7d92fc20-a1ca-11e7-9e43-df630275a618'
 # PORT = '/dev/ttyUSB0'
 PORT = 'COM13'
@@ -47,15 +70,18 @@ BAUD_RATE = 9600
 # Data capture and upload interval in seconds. Less interval will eventually hang the DHT22.
 INTERVAL = 2
 fsrValue = 0
-sensor_data = {'fsr': 0}
+
 next_reading = time.time()
 client = mqtt.Client()
+client_2 = mqtt.Client()
 # Set access token
-client.username_pw_set(ACCESS_TOKEN)
+client.username_pw_set(ACCESS_TOKEN_1)
+client_2.username_pw_set(ACCESS_TOKEN_2)
 # Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
 client.connect(THINGSBOARD_HOST, 1883, 60)
+client_2.connect(THINGSBOARD_HOST, 1883, 60)
 client.loop_start()
-
+client_2.loop_start()
 # Open serial port
 ser = serial.Serial(PORT, BAUD_RATE)
 # When a Dispatch is created with a serial port, it will automatically
